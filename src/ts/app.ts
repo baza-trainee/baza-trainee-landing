ee5282c04233b65c93605f36f0dd1ce775ad24de
PelekhIgor <igorpelekh0112@gmail.com>
2023-05-31 15:36:07 +0300

    add rewiews

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,6 +1,9 @@
 import { langMenuToggle } from './functions/langFunctions';
 import { sliderStart } from './functions/helpers';
+import { reviews } from './functions/reviews';
 
 langMenuToggle();
 
 sliderStart();
+
+reviews();
