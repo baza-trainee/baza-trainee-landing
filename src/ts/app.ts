c530d93d32312ec6b84d3cb990d8a64009834bd6
didenko.dma <didenko.dma@gmail.com>
2023-06-02 11:59:30 +0300

    Add countes to pj

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,8 +1,11 @@
 import { langMenuToggle } from './functions/languages';
 import { sliderMainStart, sliderReviewsStart } from './functions/helpers';
+import { achievementsCounters } from './functions/counter';
 
 langMenuToggle();
 
 sliderMainStart();
 
 sliderReviewsStart();
+
+achievementsCounters();
