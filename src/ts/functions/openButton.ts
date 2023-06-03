9c83e6c4403392e5686172f4ef44d26e771d305c
Diana Forostiana <dianaforost@gmail.com>
2023-06-03 20:18:35 +0200

    Added open card menu
    Додала логіку відкриття та закриття доддкової інформації про команду на карточках з поміткою "Завершено". Стилізувала скроллбар, на посилання членів команди мають бути їх аккаунти в linkedIn, поки що поставила як заглушку посилання на головну сторінку linkedIn. На кожне посилання поставила target="_blank" щоб користувач не втрачав зв'язок з нашим сайтом і посилання відкривалось на наступній вкладці. Також додала referrerpolicy="no-referrer" задля безпеки. У стилях скроллбара додала стилі для Firefox, але не тестила.

---
+++ src/ts/functions/openButton.ts
@@ -0,0 +1,25 @@
+const teamButtons = document.querySelectorAll(".projects-section__team__btn");
+const closeButton = document.querySelector(".thumb__close-icon");
+let openProject: Element | null = null;
+teamButtons.forEach((button) =>{
+    button.addEventListener("click" , (e) =>{
+        const btn = e.currentTarget as HTMLElement;
+        const project = btn.closest(".projects-section__projects-item") as HTMLElement;
+        const closeButton = project.querySelector(".thumb__close-icon");
+        if (project) {
+                        const projectInfo = project.querySelector('.thumb') as HTMLElement;
+                        if (projectInfo) {
+                            const content = project.querySelector(".projects-section__projects-item-container") as HTMLElement;
+                            
+                            projectInfo.style.top = '-464px';
+                            content.style.display = "none";
+                            openProject = project;
+                            if(closeButton){
+                                    closeButton.addEventListener("click", ()=>{
+                                    projectInfo.style.top = '77px';
+                                    content.style.display= "block";
+                                })
+                        }}
+                    }
+                })
+            })
