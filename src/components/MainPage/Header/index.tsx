a748be0cbf0b4f47f25143d586f06212b49fc4e3
unknown <mazayw@gmail.com>
2023-06-06 23:28:45 +0300

    feat: add language selector  handler

--- src/components/MainPage/Header/index.tsx
+++ src/components/MainPage/Header/index.tsx
@@ -1,6 +1,30 @@
+import { useState } from 'react';
 import styles from './styles.module.scss';
 
 const Header = () => {
+  const [activeLang, setActiveLang] = useState('UA');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const navLinks = [
+    { id: 1, title: 'Проєкти', href: '#' },
+    { id: 2, title: 'Партнери', href: '#' },
+    { id: 3, title: 'Взяти участь', href: '#' },
+    { id: 4, title: 'Контакти', href: '#' },
+  ];
+
+  const languageOptions = [
+    { id: 1, lang: 'EN' },
+    { id: 2, lang: 'PL' },
+  ];
+
+  const handleLanguageClick = (lang: string) => {
+    setActiveLang(lang);
+    handleMenuClick();
+  };
+
+  const handleMenuClick = () => {
+    setIsMenuOpen((state) => !state);
+  };
   return (
     <header className={styles.header} id="header">
       <div className={`${styles.header__container} container`}>
@@ -12,45 +36,41 @@ const Header = () => {
           </a>
         </div>
         <ul className={styles['header__nav']}>
-          <li className={styles['header__nav-elem']}>
-            <a href="#" className={styles['header__nav-link']}>
-              Проєкти
-            </a>
-          </li>
-          <li className={styles['header__nav-elem']}>
-            <a href="#" className={styles['header__nav-link']}>
-              Партнери
-            </a>
-          </li>
-          <li className={styles['header__nav-elem']}>
-            <a href="#" className={styles['header__nav-link']}>
-              Взяти участь
-            </a>
-          </li>
-          <li className={styles['header__nav-elem']}>
-            <a href="#" className={styles['header__nav-link']}>
-              Контакти
-            </a>
-          </li>
+          {navLinks.map((link) => (
+            <li key={link.id} className={styles['header__nav-elem']}>
+              <a href={link.href} className={styles['header__nav-link']}>
+                {link.title}
+              </a>
+            </li>
+          ))}
         </ul>
         <div className={styles['header__lang']}>
-          <button className={styles['header__lang-btn']} id="active-lang">
-            UA
+          <button
+            className={styles['header__lang-btn']}
+            id="active-lang"
+            onClick={handleMenuClick}
+          >
+            {activeLang}
             <svg className={styles['header__lang-arrow']}>
               <use href="@img/sprite.svg#arrow-bottom"></use>
             </svg>
           </button>
-          <ul className={styles['header__lang-list']} id="lang-list">
-            <li className={styles['header__lang-elem']}>
-              <button className={styles['header__lang-btn']}>
-                <span>EN</span>
-              </button>
-            </li>
-            <li className={styles['header__lang-elem']}>
-              <button className={styles['header__lang-btn']}>
-                <span>PL</span>
-              </button>
-            </li>
+          <ul
+            className={`${styles['header__lang-list']} ${
+              isMenuOpen && styles['header__lang-list--visible']
+            }`}
+            id="lang-list"
+          >
+            {languageOptions.map((option) => (
+              <li key={option.id} className={styles['header__lang-elem']}>
+                <button
+                  className={styles['header__lang-btn']}
+                  onClick={() => handleLanguageClick(option.lang)}
+                >
+                  <span>{option.lang}</span>
+                </button>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
