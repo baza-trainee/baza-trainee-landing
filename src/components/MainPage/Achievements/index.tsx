cb8ee60117be72627291910636c617ae539999fa
unknown <mazayw@gmail.com>
2023-06-06 19:57:07 +0300

    feat: add counter to achievment component

--- src/components/MainPage/Achievements/index.tsx
+++ src/components/MainPage/Achievements/index.tsx
@@ -1,25 +1,65 @@
+import counterHandler from '@/utils/counterHandler';
+import { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.scss';
 
+const projects = 7;
+const members = 420;
+const haveJob = 212;
+
 const Achievements = () => {
+  const componentRef = useRef(null);
+  const [projectsCount, setProjectsCount] = useState(0);
+  const [membersCount, setMembersCount] = useState(0);
+  const [haveJobCount, setHaveJobCount] = useState(0);
+  const [isPlusVisible, setIsPlusVisible] = useState(false);
+
+  const handleIntersection = (entries: IntersectionObserverEntry[]) => {
+    if (entries[0].isIntersecting) {
+      counterHandler(projectsCount, projects, setProjectsCount, () =>
+        setIsPlusVisible(true)
+      );
+      counterHandler(membersCount, members, setMembersCount);
+      counterHandler(haveJobCount, haveJob, setHaveJobCount);
+    }
+  };
+
+  useEffect(() => {
+    const options = {
+      threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    if (componentRef.current) {
+      observer.observe(componentRef.current);
+    }
+
+    return () => {
+      if (componentRef.current) {
+        observer.unobserve(componentRef.current);
+      }
+    };
+  }, []);
   return (
-    <section className={styles.achievements}>
+    <section className={styles.achievements} ref={componentRef}>
       <div className="container">
         <ul className={styles.achievements__list}>
           <li className={styles.achievements__item}>
             <p className={styles.achievements__number}>
-              <span counter-num="7">0</span>
+              <span>{projectsCount}</span>
             </p>
             <p className={styles.achievements__text}>Виконаних проєктів</p>
           </li>
           <li className={styles.achievements__item}>
             <p className={styles.achievements__number}>
-              <span counter-num="420">0</span>+
+              <span>{membersCount}</span>
+              {isPlusVisible && '+'}
             </p>
             <p className={styles.achievements__text}>Залучених учасників</p>
           </li>
           <li className={styles.achievements__item}>
             <p className={styles.achievements__number}>
-              <span counter-num="212">0</span>
+              <span>{haveJobCount}</span>
             </p>
             <p className={styles.achievements__text}>Працевлаштовано</p>
           </li>
