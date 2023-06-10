b67508d5125a3a56c3efee0a3c5556f89c8f461e
Diana Forostiana <dianaforost@gmail.com>
2023-06-10 21:35:48 +0200

    fix: project card

--- src/components/common/ProjectCard/ProjectCardTeam/index.tsx
+++ src/components/common/ProjectCard/ProjectCardTeam/index.tsx
@@ -8,35 +8,55 @@ const ProjectCardTeam = ({
   onClick: () => void;
   project: { stack: string[]; teamMembers: object[] };
 }) => {
+  const roles = [
+    {
+      id: 1,
+      role: 'PM',
+      text: 'Project Manager',
+      class: 'projects-section__projects-item__state-completed',
+    },
+    {
+      id: 2,
+      role: 'Design',
+      text: 'Design',
+      class: 'projects-section__projects-item__state-under-development',
+    },
+    {
+      id: 3,
+      role: 'Front',
+      text: 'Front-end',
+      class: 'projects-section__projects-item__state-formation-of-the-team',
+    },
+    {
+      id: 4,
+      role: 'Back',
+      text: 'Back-end',
+      class: 'projects-section__projects-item__state-formation-of-the-team',
+    },
+    {
+      id: 5,
+      role: 'QA',
+      text: 'Quality Assurance',
+      class: 'projects-section__projects-item__state-formation-of-the-team',
+    },
+  ];
   return (
-    <div className={styles['thumb']} id="thumb">
+    <div className={styles['thumb']}>
       <div className={styles['thumb__text-container']}>
         <p>Stack: {project.stack.join(', ')}</p>
-        <button
-          className={styles['thumb__close-icon']}
-          id="close-icon"
-          onClick={onClick}
-        >
+        <button className={styles['thumb__close-icon']} onClick={onClick}>
           <CloseIcon />
         </button>
       </div>
       <div className={styles['thumb__container']}>
-        <div className={styles['thumb__container__body']}>
-          <h4 className={styles['thumb__container__body__title']}>
-            Project Manager
-          </h4>
-          <RenderTeamMembersByRole roleName="PM" project={project} />
-          <h4 className={styles['thumb__container__body__title']}>Design</h4>
-          <RenderTeamMembersByRole roleName="Design" project={project} />
-          <h4 className={styles['thumb__container__body__title']}>Front-end</h4>
-          <RenderTeamMembersByRole roleName="Front" project={project} />
-          <h4 className={styles['thumb__container__body__title']}>Back-end</h4>
-          <RenderTeamMembersByRole roleName="Back" project={project} />
-          <h4 className={styles['thumb__container__body__title']}>
-            Quality Assurance
-          </h4>
-          <RenderTeamMembersByRole roleName="QA" project={project} />
-        </div>
+        {roles.map((role) => (
+          <div className={styles['thumb__container__body']} key={role.id}>
+            <h4 className={styles['thumb__container__body__title']}>
+              {role.text}
+            </h4>
+            <RenderTeamMembersByRole roleName={role.role} project={project} />
+          </div>
+        ))}
       </div>
     </div>
   );
