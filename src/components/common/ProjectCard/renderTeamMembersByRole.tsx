03092f5beabb00e8b9814de6b0e8a4413605e960
Diana Forostiana <dianaforost@gmail.com>
2023-06-10 21:15:25 +0200

    fix: nextjs-image

--- src/components/common/ProjectCard/renderTeamMembersByRole.tsx
+++ src/components/common/ProjectCard/renderTeamMembersByRole.tsx
@@ -1,15 +1,18 @@
 import styles from './ProjectCardTeam/styles.module.scss';
-const RenderTeamMembersByRole = (
-  roleName: string,
-  project: { teamMembers: object[] }
-) => {
+const RenderTeamMembersByRole = ({
+  roleName,
+  project,
+}: {
+  roleName: string;
+  project: { teamMembers: object[] };
+}) => {
   const members = project.teamMembers.filter(
     (member: any) => member.role.name === roleName
   );
   return (
     <ul className={styles['thumb__container__body__members']}>
       {members.map((member: any) => (
-        <li key={member.id}>
+        <li key={member.user._id}>
           <a
             className={styles['thumb__container__body__link']}
             href={member.user.link}
