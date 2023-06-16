bdda406086f520ec96c809e12c5c19937ac5a4ba
unknown <mazayw@gmail.com>
2023-06-16 21:06:01 +0300

    feat: add spinner

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,12 @@
+import Spinner from '../../components/common/icons/Spinner/index';
 const admin = () => {
-  return <div>admin</div>;
+  return (
+    <div>
+      <Spinner title="Loading" />
+      <Spinner title="Завантаження" />
+      <Spinner title="Pobierz" />
+    </div>
+  );
 };
 
 export default admin;
