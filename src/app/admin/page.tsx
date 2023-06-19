e5c9a73b6d00449eb57734869663353e1ba9f3ed
unknown <mazayw@gmail.com>
2023-06-20 01:58:08 +0300

    refactor: spinner icon

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,5 @@
-import Spinner from '../../components/common/icons/Spinner/index';
 const admin = () => {
-  return (
-    <div>
-      <Spinner title="Loading" />
-      <Spinner title="Завантаження" />
-      <Spinner title="Pobierz" />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default admin;
