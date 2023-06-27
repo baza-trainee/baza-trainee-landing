fa15c329eb7623523b0fa204db0c6dc7b5a59874
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-06-27 16:10:33 +0300

    feat: refactor globalStore, fix langSelector

--- src/components/common/icons/index.ts
+++ src/components/common/icons/index.ts
@@ -1,3 +1,16 @@
+export { default as ContactIcon } from './ContactIcon';
+export { default as CounterIcon } from './CounterIcon';
+export { default as FacebookIcon } from './FacebookIcon';
+export { default as LinkedinIcon } from './LinkedInIcon';
+export { default as LogOutIcon } from './LogOutIcon';
+export { default as PartnerIcon } from './Partner';
+export { default as ProjectIcon } from './ProjectIcon';
+export { default as RecallIcon } from './RecallIcon';
+export { default as ReportIcon } from './ReportIcon';
+export { default as SettingsIcon } from './SettingsIcon';
+export { default as SliderIcon } from './SliderIcon';
+export { default as SliderMenuArrow } from './SliderMenuArrow';
+
 export * from './ArrowBottomIcon';
 export * from './LogoBlack';
 export * from './CloseMainIcon';
