04b4527f8ad62f14384875840bf0f6f16f8625b6
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-06-30 00:35:54 +0300

    fix: fix slider text width;

--- src/components/MainPage/HeroSlider/SingleSlide.tsx
+++ src/components/MainPage/HeroSlider/SingleSlide.tsx
@@ -8,7 +8,7 @@ export const SingleSlide = ({ slideData }: { slideData: TSlide }) => {
       className="flex h-[50.4rem] items-center justify-center bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${image})` }}
     >
-      <div className="w-2/3 text-center text-white">
+      <div className="w-2/3 max-w-[79rem] text-center text-white">
         <h2 className="mb-5 text-[3.8rem] font-bold">{title}</h2>
         <p className="text-[2rem] font-medium leading-[1.6]">{description}</p>
       </div>
