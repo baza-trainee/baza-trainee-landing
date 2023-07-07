1f6a53453a06fdfeca5cb3ffae7eec755a6fa9e2
Ihor <mazayw@gmail.com>
2023-07-07 15:41:00 +0300

    Fondy (#45)
    * feat: add modal window handlers
    
    * feat: add payment redirect
    
    * fix: modal window text
    
    * fix: fonts
    
    * fix: fonts, minor fixes
    
    * fix: fonts
    
    * fix: zero payment processing
    
    * fix: zero payment processing
    
    * refactor: move payment logic to the hook
    
    * fix: pr fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,20 @@
 import { StoreProvider } from '@/store/globalContext';
-import { exo2, inter } from './fonts';
+import { Exo_2 } from 'next/font/google';
 
-import './globals.css';
-import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import './globals.css';
 
 export const metadata = {
   title: 'Baza-Trainee',
   description: 'Baza-Trainee',
 };
+
+const exo2 = Exo_2({
+  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ['cyrillic', 'latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({
   children,
@@ -18,9 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <StoreProvider>
-        <body className={`${inter.variable} ${exo2.className}`}>
-          {children}
-        </body>
+        <body className={exo2.className}>{children}</body>
       </StoreProvider>
     </html>
   );
