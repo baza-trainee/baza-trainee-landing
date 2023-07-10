dea1b7f02dba6f0016b6f901af90b2865dc390bf
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-07-10 13:55:04 +0300

    fix: code style;

--- src/components/atomic/buttons/AdminPanelMenuItem.tsx
+++ src/components/atomic/buttons/AdminPanelMenuItem.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react';
+
 import sidebarSectionsList from '@/components/Admin/Sidebar/sidebarSectionsList';
 import { MultiArrow } from '@/components/common/icons';
-import { FC, ReactNode } from 'react';
 
 type TProps = {
   onClick?: () => void;
@@ -21,26 +22,28 @@ export const AdminPanelMenuItem: FC<TProps> = ({
     gap-[0.8rem] items-center rounded-[0.4rem] overflow-hidden hover:bg-yellow-500 ${className}`;
 
   return (
-    <div className='relative'>
+    <div className="relative">
+      <button className={buttonClasses}>
+        <div className={`h-[2.4rem] w-[2.4rem] `}>
+          <IconComponent />
+        </div>
 
-    <button className={buttonClasses}>
-      <div className={`h-[2.4rem] w-[2.4rem] `}>
-        <IconComponent />
-      </div>
-
-      <span
-        className={`transition-transform ${iconOnly ? 'scale-0' : 'scale-100'}`}
+        <span
+          className={`transition-transform 
+          ${iconOnly ? 'scale-0' : 'scale-100'}`}
         >
-        {text}
-      </span>
-
+          {text}
+        </span>
       </button>
 
       {submenu && (
-        <div className={`absolute top-1/2 -translate-y-1/2 ${iconOnly ? '-right-[2rem]' :'right-[1.2rem]'}`}>
+        <div
+          className={`absolute top-1/2 -translate-y-1/2 
+          ${iconOnly ? '-right-[2rem]' : 'right-[1.2rem]'}`}
+        >
           <MultiArrow direction={'right'} />
         </div>
       )}
-      </div>
+    </div>
   );
 };
