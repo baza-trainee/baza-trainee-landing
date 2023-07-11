334ab32cd117f6b28550fe44bd6e087d13bf6305
WILSONPLUR <yourshows12@gmail.com>
2023-07-11 23:05:13 +0300

    feat: switching from static to dynamic data

--- src/components/MainPage/HeroSlider/slide.tsx
+++ src/components/MainPage/HeroSlider/slide.tsx
@@ -1,20 +1,32 @@
 // import 'slick-carousel/slick/slick-theme.css';
 // import 'slick-carousel/slick/slick.css';
-import { TSlide } from '.';
 import styles from './styles.module.scss';
 
-const Slide = ({ slideData }: { slideData: TSlide }) => {
-  const { image, title, text } = slideData;
+const Slide = ({
+  slideData,
+  slideLang,
+}: {
+  slideData: {
+    __v: number;
+    _id: string;
+    imageUrl: string;
+    subtitle: { en: string; pl: string; ua: string };
+    title: { en: string; pl: string; ua: string };
+  };
+  slideLang: 'en' | 'ua' | 'pl';
+}) => {
+  const { imageUrl, title, subtitle } = slideData;
   return (
     <div
       className={styles['slider-section__slide']}
-      style={{ backgroundImage: `url(${image})` }}
+      // style={{ backgroundImage: `url(${imageUrl})` }}
+      style={{ background: 'red' }}
     >
       <div className={`container ${styles['slider-section__slide-container']}`}>
         <div className={styles['slider-section__content']}>
-          <h2 className="text-white">{title}</h2>
+          <h2 className="text-white">{title[slideLang]}</h2>
           <p className="text-[2rem] font-medium leading-[1.6] text-white">
-            {text}
+            {subtitle[slideLang]}
           </p>
         </div>
       </div>
