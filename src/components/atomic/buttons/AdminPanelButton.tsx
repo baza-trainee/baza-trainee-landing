597cdcfe80b05afc630a41e995a58cdf6b122274
unknown <mazayw@gmail.com>
2023-07-16 21:38:39 +0300

    fix: types

--- src/components/atomic/buttons/AdminPanelButton.tsx
+++ src/components/atomic/buttons/AdminPanelButton.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
-import { TButtonProps } from '../types';
 
 const primaryEnabled = 'bg-neutral-75 text-neutral-300';
-const primaryDisabled = 'bg-neutral-800 text-white hover:bg-white hover:text-neutral-800';
+const primaryDisabled =
+  'bg-neutral-800 text-white hover:bg-white hover:text-neutral-800';
 const secondaryEnabled = 'border-neutral-300 text-neutral-300';
-const secondaryDisabled = 'border-neutral-800 bg-white text-neutral-800 hover:bg-neutral-800 hover:text-white';
+const secondaryDisabled =
+  'border-neutral-800 bg-white text-neutral-800 hover:bg-neutral-800 hover:text-white';
 
-export const AdminPanelButton: FC<TButtonProps> = ({
+export const AdminPanelButton: FC<any> = ({
+  //fix any
   children,
   className = '',
   variant = 'primary',
@@ -15,20 +17,34 @@ export const AdminPanelButton: FC<TButtonProps> = ({
   iconOnly,
   ...rest
 }) => {
-const buttonClasses = `relative flex-center gap-[1.2rem] h-[5.6rem] shrink-0 pr-[3.2rem]
+  const buttonClasses = `relative flex-center gap-[1.2rem] h-[5.6rem] shrink-0 pr-[3.2rem]
     ${icon ? 'pl-[1.2rem]' : 'pl-[3.2rem]'}
     ${icon && iconOnly && 'w-[4.7rem]'}
     ${variant === 'primary' && (disabled ? primaryEnabled : primaryDisabled)}
-    ${variant === 'secondary' && (disabled ? secondaryEnabled : secondaryDisabled)}
-    rounded-[0.4rem] border text-[2.2rem] font-semibold overflow-hidden ${className}`
+    ${
+      variant === 'secondary' &&
+      (disabled ? secondaryEnabled : secondaryDisabled)
+    }
+    rounded-[0.4rem] border text-[2.2rem] font-semibold overflow-hidden ${className}`;
 
   return (
-    <button className={buttonClasses} disabled={disabled} {...rest}>
-      {icon && <div className={`h-[2.4rem] w-[2.4rem] ${iconOnly && "block-center"}`}>
-        {icon}
-      </div>}
-      
-      <span className={`transition-transform ${icon && iconOnly ? 'scale-0' : 'scale-100'}`}>
+    <button
+      className={buttonClasses}
+      disabled={disabled}
+      type="button"
+      {...rest}
+    >
+      {icon && (
+        <div className={`h-[2.4rem] w-[2.4rem] ${iconOnly && 'block-center'}`}>
+          {icon}
+        </div>
+      )}
+
+      <span
+        className={`transition-transform ${
+          icon && iconOnly ? 'scale-0' : 'scale-100'
+        }`}
+      >
         {children}
       </span>
     </button>
