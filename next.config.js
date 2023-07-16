9d3356bf145760dc32a178d96d6a4cf92b459f56
unknown <mazayw@gmail.com>
2023-07-17 00:19:23 +0300

    feat: add proxy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,30 @@
-/** @type {import('next').NextConfig} 
-const nextConfig = {};
-module.exports = nextConfig;*/
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const path = require('path');
+const url = process.env.NEXT_PUBLIC_PROXY_URL;
 
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: `@import "@/styles/styles.scss";`,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: `${url}/api/v1/:path*`,
+      },
+    ];
+  },
+  async middleware() {
+    const proxy = createProxyMiddleware({
+      target: url,
+      changeOrigin: true,
+      cookieDomainRewrite: '', // Enable cookie sharing
+      secure: true, // If the backend is not using HTTPS
+    });
+
+    return {
+      '/api/v1': proxy,
+    };
   },
 };
