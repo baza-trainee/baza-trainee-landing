dd4022b923800db5a3f6f2d75079ad8b15de35e4
unknown <mazayw@gmail.com>
2023-07-17 21:20:30 +0300

    refactor: change file structure

--- src/app/admin/documents/page.tsx
+++ src/app/admin/documents/page.tsx
@@ -1,4 +1,4 @@
-import { Documents } from '@/components/AdminPage/Documents/index';
+import { Documents } from '@/components/AdminPage/Documents';
 export default function Docs() {
   return <Documents />;
 }
