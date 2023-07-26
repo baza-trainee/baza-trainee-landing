8e18d1455ca951ed35235d584ec46d9f5fb8d048
unknown <mazayw@gmail.com>
2023-07-27 00:05:59 +0300

    fix: partners adaptive

--- src/components/MainPage/Partners/index.tsx
+++ src/components/MainPage/Partners/index.tsx
@@ -17,52 +17,67 @@ const sliderSettings = {
   slidesToScroll: 1,
   infinite: false,
   draggable: false,
+  responsive: [
+    {
+      breakpoint: 1300,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        rows: 2,
+      },
+    },
+  ],
 };
 
 export const Partners = () => {
   return (
     <section id="partners">
       <ContainerMaxW1200 className="flex-col">
-        <h2 className="mb-[3.2rem] text-center text-6xl font-bold">Партнери</h2>
+        <h2 className=" text-center text-6xl font-bold">Партнери</h2>
 
         <Slider {...sliderSettings}>
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>1</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>2</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>3</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>4</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>5</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>6</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>7</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>8</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>9</div>
           </Link>
 
-          <Link className="" href="#">
+          <Link className="mt-[3.2rem]" href="#">
             <div className={nestedElemStyle}>10</div>
           </Link>
         </Slider>
