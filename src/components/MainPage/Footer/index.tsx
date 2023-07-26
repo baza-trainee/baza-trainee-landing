3a5004137cf8c49b206263665a5b7fef3ea27907
unknown <mazayw@gmail.com>
2023-07-27 01:03:15 +0300

    fix: footer adaptive

--- src/components/MainPage/Footer/index.tsx
+++ src/components/MainPage/Footer/index.tsx
@@ -52,74 +52,77 @@ export const Footer = async () => {
   return (
     <footer className="bg-neutral-700 pb-12 pt-16" id="footer">
       <ContainerMaxW1200 className="flex-col text-white">
-        <nav className="flex flex-col justify-between gap-[3.2rem] sm:min-h-[18.4rem] sm:flex-row ">
-          <Link href={'/'} className="lg:w-full ">
+        <nav className="flex flex-col justify-between gap-[3.2rem] sm:min-h-[18.4rem] sm:flex-row">
+          <Link href={'/'} className=" w-96">
             <LogoMain className="h-32 w-32 md:h-[12.4rem] md:w-[12.4rem]" />
           </Link>
+          <div className="flex w-full flex-col flex-wrap justify-between gap-[3.2rem] sm:min-h-[18.4rem] sm:flex-row ">
+            <div className="flex flex-col gap-4 lg:w-64">
+              {anchorLinksList.map(({ title, href }) => (
+                <Link
+                  href={href}
+                  key={title + href}
+                  className="visited:text-yellow-800 hover:text-yellow-500"
+                >
+                  {title}
+                </Link>
+              ))}
+            </div>
 
-          <div className="flex flex-col gap-4 lg:w-full">
-            {anchorLinksList.map(({ title, href }) => (
-              <Link
-                href={href}
-                key={title + href}
-                className="visited:text-yellow-800 hover:text-yellow-500"
-              >
-                {title}
-              </Link>
-            ))}
-          </div>
+            <div className="flex flex-col gap-4 lg:w-96">
+              {officialDocsList.map(({ title, href }, i) => (
+                <Link
+                  key={title + href}
+                  href={href}
+                  scroll={false}
+                  className={`${
+                    i < 2 ? 'underline underline-offset-8' : ''
+                  } visited:text-yellow-800 hover:text-yellow-500`}
+                >
+                  {title}
+                </Link>
+              ))}
+            </div>
 
-          <div className="flex flex-col gap-4 lg:w-full">
-            {officialDocsList.map(({ title, href }, i) => (
-              <Link
-                key={title + href}
-                href={href}
-                scroll={false}
-                className={`${
-                  i < 2 ? 'underline underline-offset-8' : ''
-                } visited:text-yellow-800 hover:text-yellow-500`}
-              >
-                {title}
-              </Link>
-            ))}
-          </div>
+            <div className="flex flex-col justify-between gap-4 sm:w-full sm:flex-row lg:w-auto lg:flex-col">
+              <div className="flex flex-col gap-4 lg:w-[27.6rem]">
+                <p className="flex gap-3">
+                  <PhoneIcon />
+                  <span>
+                    {formatPhoneNumber(contactsInfo.contactsDataList.phone1)}
+                  </span>
+                </p>
+                <p className="flex gap-3">
+                  <PhoneIcon />
+                  <span>
+                    {formatPhoneNumber(contactsInfo.contactsDataList.phone2)}
+                  </span>
+                </p>
+                <p className="flex gap-3">
+                  <MailIcon />
+                  <span>{contactsInfo.contactsDataList.email}</span>
+                </p>
+              </div>
 
-          <div className="flex flex-col gap-4 lg:w-full">
-            <p className="flex gap-3">
-              <PhoneIcon />
-              <span>
-                {formatPhoneNumber(contactsInfo.contactsDataList.phone1)}
-              </span>
-            </p>
-            <p className="flex gap-3">
-              <PhoneIcon />
-              <span>
-                {formatPhoneNumber(contactsInfo.contactsDataList.phone2)}
-              </span>
-            </p>
-            <p className="flex gap-3">
-              <MailIcon />
-              <span>{contactsInfo.contactsDataList.email + ''}</span>
-            </p>
-
-            <div className="mt-[3.2rem] flex gap-[3.2rem] sm:mt-auto">
-              <Link
-                href={contactsInfo.socialsMediaList.linkedin}
-                target="blank"
-              >
-                <LinkedInIcon className="fill-yellow-500 hover:fill-yellow-700 active:fill-yellow-800" />
-              </Link>
-              <Link
-                href={contactsInfo.socialsMediaList.telegram}
-                target="blank"
-              >
-                <TelegramIcon className="fill-yellow-500 hover:fill-yellow-700 active:fill-yellow-800" />
-              </Link>
+              <div className="mt-[3.2rem] flex w-96 gap-[3.2rem] sm:mt-auto">
+                <Link
+                  href={contactsInfo.socialsMediaList.linkedin}
+                  target="blank"
+                >
+                  <LinkedInIcon className="fill-yellow-500 hover:fill-yellow-700 active:fill-yellow-800" />
+                </Link>
+                <Link
+                  href={contactsInfo.socialsMediaList.telegram}
+                  target="blank"
+                >
+                  <TelegramIcon className="fill-yellow-500 hover:fill-yellow-700 active:fill-yellow-800" />
+                </Link>
+              </div>
             </div>
           </div>
         </nav>
 
-        <span className="mt-12 text-[1.4rem] text-neutral-75 sm:mt-0">
+        <span className="mt-12 text-[1.4rem] text-neutral-75 sm:mt-10">
           Розробка BazaTraineeUkraine 2023 Усі права захищені.
         </span>
       </ContainerMaxW1200>
