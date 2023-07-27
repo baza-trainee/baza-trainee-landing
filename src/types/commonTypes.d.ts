8441885e16cce7be8c6e7a32dc06e35fcfc0b658
unknown <mazayw@gmail.com>
2023-07-28 00:43:20 +0300

    fix: types

--- src/types/commonTypes.d.ts
+++ src/types/commonTypes.d.ts
@@ -13,25 +13,4 @@ export type TSlideReview = {
   imageUrl: string;
 };
 
-export interface IProject {
-  _id: string;
-  title: string;
-  imageUrl: string;
-  status: string;
-  link: string;
-  description: string;
-  creationDate: number;
-  launchDate: number;
-  complexity: number;
-  teamMembers: Array<{
-    user: {
-      _id: string;
-      name: string;
-      link: string;
-    };
-    role: {
-      _id: string;
-      name: string;
-    };
-  }>;
-}
+
