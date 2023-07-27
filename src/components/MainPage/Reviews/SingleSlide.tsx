e8061c20ddad092b481d6efb370ec41875c3a68a
Ihor <mazayw@gmail.com>
2023-07-27 11:28:18 +0300

    Sliders fix (#64)
    * fix: hero slider arrows visibility
    
    * fix: review slider  arrows visibility
    
    * fix: minor fixes
    
    * fix: hero slider arrow position
    
    * fix: partners adaptive
    
    * fix: reviews adaptive
    
    * fix: footer adaptive
    
    * refactor: small changes in footer
    
    * feat: add scrollbar
    
    * fix: change forms text according mockup

--- src/components/MainPage/Reviews/SingleSlide.tsx
+++ src/components/MainPage/Reviews/SingleSlide.tsx
@@ -7,8 +7,8 @@ export const SingleSlide = ({ slideData }: { slideData: TSlideReview }) => {
 
   return (
     <div
-      className="min-h-48 m-auto flex w-3/5 flex-col flex-wrap items-center gap-[4.8rem]
-      text-[1.6rem] font-normal text-neutral-700 sm:flex-row md:gap-[4.8rem] lg:flex-nowrap xl:w-[80rem]"
+      className="min-h-48 m-auto flex w-4/5 flex-col flex-wrap items-center gap-[2rem]
+      text-[1.6rem] font-normal text-neutral-700 md:flex-row md:flex-nowrap md:gap-[4.8rem] xl:w-[80rem]"
     >
       <div className="relative h-48 w-48 shrink-0 overflow-hidden rounded-full">
         <Image
@@ -21,13 +21,13 @@ export const SingleSlide = ({ slideData }: { slideData: TSlideReview }) => {
         />
       </div>
 
-      <div className="w-80 whitespace-nowrap text-[2rem]">
+      <div className="w-80 self-baseline whitespace-nowrap text-[2rem] md:self-center">
         <h4 className="text-[2rem] font-medium">{name}</h4>
         <p className="text-[1.6rem]">{role}</p>
         <p className="text-[1.4rem] text-neutral-400">{date}</p>
       </div>
 
-      <span className="w-max">{review}</span>
+      <span className="w-full">{review}</span>
     </div>
   );
 };
