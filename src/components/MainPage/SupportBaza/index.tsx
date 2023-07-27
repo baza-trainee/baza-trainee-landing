bcdf7d4f50ef1d06c68e893746e7de626943073f
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-07-27 17:08:51 +0300

    fix: fix;

--- src/components/MainPage/SupportBaza/index.tsx
+++ src/components/MainPage/SupportBaza/index.tsx
@@ -1,11 +1,11 @@
 import { ContainerMaxW1200, SupportBazaButton } from '@/components/atomic';
-import { Modal, ModalContentDonate } from '../Modal';
+import { Modal } from '../Modal';
 
 export const SupportBaza = () => {
   return (
     <section>
       <ContainerMaxW1200>
-        <Modal content={<ModalContentDonate />}>
+        <Modal content="donate">
           <SupportBazaButton className="m-auto">
             Підтримати Baza Trainee Ukraine
           </SupportBazaButton>
