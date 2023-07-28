4c1296fe2bbdbce73cbb0312eb69c59390ebd082
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-07-29 01:54:06 +0300

    fix: fix;

--- src/components/atomic/buttons/SlickArrow.tsx
+++ src/components/atomic/buttons/SlickArrow.tsx
@@ -20,7 +20,9 @@ export const SlickArrow = (props: IProps) => {
       onClick={onClick}
       bigSize
       className={`
-          absolute top-[15%] z-10 -translate-y-1/2 cursor-pointer md:top-1/2 ${direction}-0
+          absolute top-[15%] z-10 -translate-y-1/2 cursor-pointer md:top-1/2
+          ${direction === 'left' && 'left-0'}
+          ${direction === 'right' && 'right-0'}
           ${direction === 'left' && currentSlide === 0 && 'hidden'}
           ${direction === 'right' && currentSlide === slideCount - 1 && 'hidden'}
         `}
