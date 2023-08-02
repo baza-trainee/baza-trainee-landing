e945462ca353ee0cf1156979437d847d860855d7
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-08-02 13:30:35 +0300

    fix: fix AdminButton;

--- src/components/atomic/buttons/AdminPanelButton.tsx
+++ src/components/atomic/buttons/AdminPanelButton.tsx
@@ -1,11 +1,9 @@
 import { ButtonHTMLAttributes, FC, ReactNode } from 'react';
 
-const primaryEnabled = 'bg-neutral-75 text-neutral-300';
-const primaryDisabled =
-  'bg-neutral-800 text-white hover:bg-white hover:text-neutral-800';
-const secondaryEnabled = 'border-neutral-300 text-neutral-300';
-const secondaryDisabled =
-  'border-neutral-800 bg-white text-neutral-800 hover:bg-neutral-500 hover:text-white';
+const primary =
+  'bg-neutral-800 text-white hover:bg-white hover:text-neutral-800 border-neutral-800 disabled:border-neutral-75 disabled:bg-neutral-75 disabled:text-neutral-300';
+const secondary =
+  'border-neutral-800 bg-white text-neutral-800 hover:bg-neutral-500 hover:text-white disabled:border-neutral-300 disabled:text-neutral-300';
 
 interface AdminPanelButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -26,15 +24,13 @@ export const AdminPanelButton: FC<AdminPanelButtonProps> = ({
   iconOnly,
   ...rest
 }) => {
-  const buttonClasses = `relative flex-center gap-[1.2rem] h-[5.6rem] shrink-0 pr-[3.2rem]
+  const buttonClasses = `
+    relative flex-center gap-[1.2rem] h-[5.6rem] shrink-0 pr-[3.2rem]
     ${icon ? 'pl-[1.2rem]' : 'pl-[3.2rem]'}
-    ${icon && iconOnly && 'w-[4.7rem]'}
-    ${variant === 'primary' && (disabled ? primaryEnabled : primaryDisabled)}
-    ${
-      variant === 'secondary' &&
-      (disabled ? secondaryEnabled : secondaryDisabled)
-    }
-    rounded-[0.4rem] border text-[2.2rem] font-semibold overflow-hidden ${className}`;
+    ${iconOnly && icon ? 'w-[4.7rem] block-center' : ''}
+    ${variant === 'primary' ? primary : secondary}
+    rounded-[0.4rem] border text-[2.2rem] font-semibold overflow-hidden transition-colors ${className}
+`;
 
   return (
     <button
@@ -44,7 +40,9 @@ export const AdminPanelButton: FC<AdminPanelButtonProps> = ({
       {...rest}
     >
       {icon && (
-        <div className={` h-[2.4rem] w-[2.4rem] ${iconOnly && 'block-center'}`}>
+        <div
+          className={`h-[2.4rem] w-[2.4rem] ${iconOnly ? 'block-center' : ''}`}
+        >
           {icon}
         </div>
       )}
