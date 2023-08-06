bf57645306d8fdfffc63546c0367b97dabe76fb2
WILSONPLUR <yourshows12@gmail.com>
2023-08-06 20:56:54 +0300

    feat: made switch language on landing page, fixed bug#72

--- src/components/atomic/buttons/MoreProjectsButton.tsx
+++ src/components/atomic/buttons/MoreProjectsButton.tsx
@@ -1,12 +1,19 @@
 import { MultiArrow } from '@/components/common/icons';
+import { TDictionary } from '@/types';
 
-export const MoreProjectsButton = ({ open = false }) => {
+export const MoreProjectsButton = ({
+  open = false,
+  dict,
+}: {
+  open: boolean;
+  dict: TDictionary;
+}) => {
   return (
     <button
       className="relative mx-auto h-32 max-w-fit rounded-md px-[0.8rem]
     py-[1.2rem] after:absolute after:left-0 after:top-1/2 after:w-full after:scale-x-0 after:border-t after:border-neutral-800 after:transition-all after:content-[''] after:hover:scale-90"
     >
-      <span className="text-[2rem]">Більше проєктів</span>
+      <span className="text-[2rem]">{dict.moreProjects}</span>
 
       <MultiArrow direction="bottom" className="mx-auto mt-3" />
     </button>
