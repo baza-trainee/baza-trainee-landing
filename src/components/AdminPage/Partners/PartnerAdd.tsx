4127751a1d3fde9118f11dfc1a5f11894ab84838
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-08-14 12:30:54 +0300

    fix: change logic after adding a new partner

--- src/components/AdminPage/Partners/PartnerAdd.tsx
+++ src/components/AdminPage/Partners/PartnerAdd.tsx
@@ -19,12 +19,6 @@ export const PartnerAdd = ({ cancelAdd }: PartnerAddProps) => {
     homeUrl: '',
   });
 
-  const resetFormHandler = () => {
-    handleFieldChange('name', '');
-    handleFieldChange('file', null);
-    handleFieldChange('homeUrl', '');
-  };
-
   const handleSubmit = () => {
     if (!isFormValid) {
       setAlertInfo({
@@ -44,15 +38,15 @@ export const PartnerAdd = ({ cancelAdd }: PartnerAddProps) => {
         state: 'success',
         title: 'Успіх',
         textInfo: 'Ваші дані успішно збережено.',
+        func: cancelAdd,
       });
       dispatch(newPartner);
-      resetFormHandler();
     }
   };
 
   return (
-    <div className="w-full bg-base-light">
-      <AdminTitle className="mb-[2.4rem] ml-[0.4rem] mt-[0.9rem] tracking-wide">
+    <div className='w-full bg-base-light'>
+      <AdminTitle className='mb-[2.4rem] ml-[0.4rem] mt-[0.9rem] tracking-wide'>
         Додати партнера
       </AdminTitle>
       <PartnerForm
@@ -61,15 +55,15 @@ export const PartnerAdd = ({ cancelAdd }: PartnerAddProps) => {
         isFormValid={isFormValid}
         handleFieldChange={handleFieldChange}
       />
-      <div className="ml-3.5 flex gap-[1.5rem] pt-[2.2rem]">
+      <div className='ml-3.5 flex gap-[1.5rem] pt-[2.2rem]'>
         <AdminPanelButton
-          type="submit"
-          className="pl-[6.8rem] pr-[6.8rem]"
+          type='submit'
+          className='pl-[6.8rem] pr-[6.8rem]'
           onClick={handleSubmit}
         >
           Додати
         </AdminPanelButton>
-        <AdminPanelButton onClick={cancelAdd} variant="secondary">
+        <AdminPanelButton onClick={cancelAdd} variant='secondary'>
           Скасувати
         </AdminPanelButton>
       </div>
