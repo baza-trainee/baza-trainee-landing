96c7cc8fb76adc7087e8e2d6007a72afc0133b2b
unknown <mazayw@gmail.com>
2023-08-17 15:49:40 +0300

    refactor: accessebility dot

--- src/components/MainPage/HeroSlider/Dots.tsx
+++ src/components/MainPage/HeroSlider/Dots.tsx
@@ -13,13 +13,17 @@ export const Dots = ({
         onClick={() => goToSlide(index)}
         aria-label={`Go to ${index + 1} slide`}
         key={`key_${index + currentSlide}`}
-        className={`relative rounded-full border border-neutral-800 hover:cursor-pointer 
-          ${
-            index === currentSlide
-              ? 'h-[2rem] w-[2rem] bg-neutral-800'
-              : 'h-[1.6rem] w-[1.6rem]'
-          }`}
-      ></button>
+        className="h-[5rem] w-[5rem] sm:h-[2rem] sm:w-[2rem]"
+      >
+        <div
+          className={`relative rounded-full border border-neutral-800 hover:cursor-pointer 
+      ${
+        index === currentSlide
+          ? 'h-[2rem] w-[2rem] bg-neutral-800'
+          : 'h-[1.6rem] w-[1.6rem]'
+      }`}
+        ></div>
+      </button>
     ))}
   </div>
 );
