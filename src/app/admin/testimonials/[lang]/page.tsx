a33e19df1877795b24d5ebce5802ec26124d852c
unknown <mazayw@gmail.com>
2023-08-21 00:43:33 +0300

    feat: language normalization

--- src/app/admin/testimonials/[lang]/page.tsx
+++ src/app/admin/testimonials/[lang]/page.tsx
@@ -1,9 +1,13 @@
 import { Testimonials_main } from '@/components/AdminPage/Testimonials';
 
+export async function generateStaticParams() {
+  return [{ lang: 'en' }, { lang: 'pl' }, { lang: 'ua' }];
+}
+
 export default function Testimonials({
   params: { lang },
 }: {
-  params: { lang: string };
+  params: { lang: 'en' | 'pl' | 'ua' };
 }) {
   return <Testimonials_main lang={lang} />;
 }
--- src/components/MainPage/Header/LanguageSelector.tsx
+++ src/components/MainPage/Header/LanguageSelector.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { GlobalContext, TLandingLanguage } from '@/store/globalContext';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { MultiArrow } from '@/components/common/icons';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 
 const languageOptions: TLandingLanguage[] = ['ua', 'en', 'pl'];
+
+type TLang = 'ua' | 'en' | 'pl';
 
 const btnStyle =
   'z-10 flex cursor-pointer items-center gap-3 bg-transparent pl-5 text-[2rem] font-semibold h-[3.3rem] w-32';
@@ -16,20 +19,25 @@ const underLineStyle =
 
 export const LanguageSelector = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const router = useRouter();
+  const [curLang, setCurLang] = useState<TLang>('ua');
+
   const { landingLanguage, setLandingLanguage } = useContext(GlobalContext);
 
-  const getLang = () => {
-    const url = new URL(window.location.href).pathname;
-    const segments = url.split('/');
-    const languageCode = segments[segments.length - 1];
-    return languageCode;
-  };
-  const language = getLang();
+  useEffect(() => {
+    const getLang = () => {
+      const langArr = ['en', 'pl', 'ua'];
 
-  const handleLanguageClick = (lang: TLandingLanguage) => {
-    setLandingLanguage(lang);
-    handleMenuClick();
-  };
+      const url = new URL(window.location.href).pathname;
+      const segments = url.split('/');
+      const languageCode = segments[segments.length - 1];
+      const isLangCorrect = langArr.includes(languageCode);
+      const normalizedLang = isLangCorrect ? languageCode : 'ua';
+      !isLangCorrect && router.replace(normalizedLang);
+      setCurLang(normalizedLang as TLang);
+    };
+    getLang();
+  }, [router]);
 
   const handleMenuClick = () => {
     setIsMenuOpen((state) => !state);
@@ -44,14 +52,14 @@ export const LanguageSelector = () => {
   return (
     <div className="relative">
       <button className={btnStyle} id="active-lang" onClick={handleMenuClick}>
-        {language.toUpperCase()}
+        {curLang.toUpperCase()}
 
         <MultiArrow direction="bottom" open={isMenuOpen} />
       </button>
 
       <ul className={menuStyle} id="lang-list">
         {languageOptions
-          .filter((lang) => lang !== language)
+          .filter((lang) => lang !== curLang)
           .map((lang) => (
             <li key={`key_${lang}`}>
               <Link href={lang} className={btnStyle}>
