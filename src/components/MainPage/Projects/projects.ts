de636a3d31ecaf91acd952af962d745cd5f0cf72
WILSONPLUR <yourshows12@gmail.com>
2023-09-04 16:25:30 +0300

    fix: added new projects

--- src/components/MainPage/Projects/projects.ts
+++ src/components/MainPage/Projects/projects.ts
@@ -4,6 +4,235 @@ export const projects = [
   {
     _id: crypto.randomUUID(),
     title: {
+      en: 'Hust',
+      pl: 'Hust',
+      ua: 'Хист',
+    },
+    imageUrl: '/img/projects/hust.webp',
+    status: {
+      en: 'Under development',
+      pl: 'W budowie',
+      ua: 'В розробці',
+    },
+    statusVal: 'under-development',
+    link: '',
+    description: {
+      en: 'Hust is an aggregator of veteran initiatives',
+      pl: 'Hust - agregator inicjatyw dla weteranów',
+      ua: 'Хист - агрегатор ветеранських ініціатив',
+    },
+    creationDate: 1690848000000,
+    launchDate: 0,
+    complexity: 3,
+    teamMembers: [
+      {
+        user: {
+          name: {
+            en: 'Olga Ivanova',
+            pl: 'Olga Iwanowa',
+            ua: 'Ольга Іванова',
+          },
+          link: 'https://www.linkedin.com/in/ivolga-kyiv/',
+        },
+        role: {
+          name: 'Product Owner',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Zhanna Belan',
+            pl: 'Zhanna Belan',
+            ua: 'Жанна Бєлан',
+          },
+          link: 'https://www.linkedin.com/in/joan-bielan-068434283/',
+        },
+        role: {
+          name: 'Project Manager',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Iryna Stolyarova',
+            pl: 'Iryna Stolarowa',
+            ua: 'Ірина Столярова',
+          },
+          link: 'https://www.linkedin.com/in/iryna-stoliarova/',
+        },
+        role: {
+          name: 'Design',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Olga Butyrska',
+            pl: 'Olga Butyrska',
+            ua: 'Ольга Бутирська',
+          },
+          link: 'https://www.linkedin.com/in/olha-butyrska/',
+        },
+        role: {
+          name: 'Design',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Julia Kain',
+            pl: 'Julia Kain',
+            ua: 'Джулія Каїн',
+          },
+          link: 'https://www.linkedin.com/in/julia-kain/',
+        },
+        role: {
+          name: 'Design',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Olexandra Terekhova',
+            pl: 'Olexandra Terekhova',
+            ua: 'Олександра Терехова',
+          },
+          link: 'https://www.linkedin.com/in/%D0%BE%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%D0%B0-%D1%82%D0%B5%D1%80%D0%B5%D1%85%D0%BE%D0%B2%D0%B0-4b483843/',
+        },
+        role: {
+          name: 'Design',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Danylo Glushko',
+            pl: 'Danylo Glushko',
+            ua: 'Данило Глушко',
+          },
+          link: '',
+        },
+        role: {
+          name: 'Front-end',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Artem Shevchuk',
+            pl: 'Artem Szewczuk',
+            ua: 'Артем Шевчук',
+          },
+          link: 'https://www.linkedin.com/in/artem-shevchuk-b1955b190/',
+        },
+        role: {
+          name: 'Front-end',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Katerina Lanina',
+            pl: 'Kateryna Łanina',
+            ua: 'Катерина Ланіна',
+          },
+          link: 'https://www.linkedin.com/in/kateryna-lanina/',
+        },
+        role: {
+          name: 'Front-end',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Danil Zdorik',
+            pl: 'Danił Zdorik',
+            ua: 'Даніл Здорик',
+          },
+          link: 'https://www.linkedin.com/in/zdoryk/',
+        },
+        role: {
+          name: 'Back-end',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Yevhen Blokhin',
+            pl: 'Jewgienij Błochin',
+            ua: 'Євген Блохін',
+          },
+          link: 'https://www.linkedin.com/in/ievgen-blokhin',
+        },
+        role: {
+          name: 'Quality Assurance',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Lilia Bakalinska',
+            pl: 'Lilia Bakalińska',
+            ua: 'Лілія Бакалінська',
+          },
+          link: 'https://www.linkedin.com/in/liliia-bakalinska/',
+        },
+        role: {
+          name: 'Quality Assurance',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Olga Savchenko',
+            pl: 'Olga Sawczenko',
+            ua: 'Ольга Савченко',
+          },
+          link: 'https://www.linkedin.com/in/olsavchenko/',
+        },
+        role: {
+          name: 'Quality Assurance',
+        },
+      },
+      {
+        user: {
+          name: {
+            en: 'Yana Glazkova',
+            pl: 'Yana Glazkova',
+            ua: 'Яна Глазкова',
+          },
+          link: 'https://www.linkedin.com/in/%D1%8F%D0%BD%D0%B0-%D0%B3%D0%BB%D0%B0%D0%B7%D0%BA%D0%BE%D0%B2%D0%B0-241424281/',
+        },
+        role: {
+          name: 'Quality Assurance',
+        },
+      },
+    ],
+  },
+  {
+    _id: crypto.randomUUID(),
+    title: '57-42',
+    imageUrl: '/img/projects/57-42.webp',
+    status: {
+      en: 'Under development',
+      pl: 'W budowie',
+      ua: 'В розробці',
+    },
+    statusVal: 'under-development',
+    link: '',
+    description: {
+      en: 'Strona docelowa - zbiórka funduszy dla 57 Brygady, 42 Batalionu',
+      pl: 'Landing page - fundraising for the 57th Brigade, 42nd Battalion',
+      ua: 'Лендінг - збір коштів для 57-ї бригади 42 батальону',
+    },
+    creationDate: 1691539200000,
+    launchDate: 0,
+    complexity: 1,
+    teamMembers: [],
+  },
+  {
+    _id: crypto.randomUUID(),
+    title: {
       en: 'Murrfecto',
       pl: 'Murrfecto',
       ua: 'Murrfecto',
