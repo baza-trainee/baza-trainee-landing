6bd14c0886ccbbf5fa7f258220eec77a82745c65
Neschadin Oleksandr <73835274+Neschadin@users.noreply.github.com>
2023-09-05 00:24:49 +0300

    feat: removed sass from the project;

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
 const url = process.env.NEXT_PUBLIC_PROXY_URL;
 
 module.exports = {
   output: 'standalone',
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "@/styles/styles.scss";`,
-  },
   experimental: {
     serverActions: true,
   },
